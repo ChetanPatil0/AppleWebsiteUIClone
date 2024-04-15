0ad0f43e855e850ede22163f5f6db4d1bbd70cab
chetanpatil0 <patilchetan2905@gmail.com>
2024-04-15 22:55:52 +0530

    add section1 module

--- src/nav.js
+++ src/nav.js
@@ -3,49 +3,33 @@ import React from "react";
 
 function Navbar() {
     return (
-
-        <header>
-            <nav>
-                <div className="logo">
-                    <a href=""><i className="bi bi-apple"></i></a>
-                </div>
-                <div className="navbar">
-                    <a >Store</a>
-                    <a href="">Mac</a>
-                    <a href="">iPad</a>
-                    <a href="">Watch</a>
-                    <a href="">Airpods</a>
-                    <a href="">TV & Home</a>
-                    <a href="">Entertainment</a>
-                    <a href="">Accessories</a>
-                    <a href="">Support</a>
-                </div>
-                <div className="cart">
-                    <a href=""><i class="bi bi-search"></i></a>
-                    <a href=""><i class="bi bi-bag"></i></a>
-                </div>
-            </nav>
-            <div className="nav-sub-menu">
-                <ul id="list1">
-                    <li className="list-head">Shop</li>
-
-                </ul>
-                <ul >
-                    <li className="list-head">Quick Links</li>
-                    <li>Find a Store</li>
-                    <li>Order Status</li>
-                    <li>Apple trade in</li>
-                    <li></li>
-                </ul>
-                <ul >
-                    <li className="list-head">Shop Special Stores</li>
-                    <li>Education</li>
-                    <li>Busines</li>
-                    <li>Government</li>
-                    <li></li>
-                </ul>
+        <>
+            <header>
+                <nav>
+                    <div className="logo">
+                        <a href=""><i className="bi bi-apple"></i></a>
+                    </div>
+                    <div className="navbar">
+                        <a >Store</a>
+                        <a href="">Mac</a>
+                        <a href="">iPad</a>
+                        <a href="">Watch</a>
+                        <a href="">Airpods</a>
+                        <a href="">TV & Home</a>
+                        <a href="">Entertainment</a>
+                        <a href="">Accessories</a>
+                        <a href="">Support</a>
+                    </div>
+                    <div className="cart">
+                        <a href=""><i class="bi bi-search"></i></a>
+                        <a href=""><i class="bi bi-bag"></i></a>
+                    </div>
+                </nav>
+            </header>
+            <div class="offers">
+                <p>Join us this Earth Day by recycling your Apple devices.‡ <span><a href="">Recycle for free {">"}</a></span></p>
             </div>
-        </header>
+        </>
     )
 }
 
---
+++ src/section1.js
@@ -0,0 +1,25 @@
+import React from "react";
+function section1_part1()
+{
+    return(
+        <>
+          <section1 className="section1">
+            <div className="part1">
+                <video autoPlay loop width={'100%'}>
+                    <source src="images/xlarge.mp4" >
+                    
+                    </source>
+                </video>
+                <div className="part1_text">
+                    <img src="images\hero_logo_apple_vision_pro.png"></img>
+                    <h2>You've never seen everything like this before.</h2>
+                    <button><a href="#">Learn more</a></button>
+                    <button><a href="#">Buy</a></button>
+                </div>
+            </div>
+          </section1>
+        </>
+    )
+}
+
+export default section1_part1;
