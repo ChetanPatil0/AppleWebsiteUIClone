d31d17e19732eab9ad65f3bddda294d13d3320a4
chetanpatil0 <patilchetan2905@gmail.com>
2024-04-16 22:50:50 +0530

    add buttons in section1js part1

--- src/section1.js
+++ src/section1.js
@@ -13,8 +13,8 @@ function section1_part1()
                 <div className="part1_text">
                     <img src="images\hero_logo_apple_vision_pro.png"></img>
                     <h2>You've never seen everything like this before.</h2>
-                    <button><a href="#">Learn more</a></button>
-                    <button><a href="#">Buy</a></button>
+                    <button className="btn-1"><a href="#">Learn more</a></button>
+                    <button className="btn-2"><a href="#" >Buy</a></button>
                 </div>
             </div>
           </section1>
