9fc56884ba0cc80fe596325b43daf5d6f98815e5
chetanpatil0 <patilchetan2905@gmail.com>
2024-05-03 20:46:01 +0530

    update section2 component

--- src/section2.js
+++ src/section2.js
@@ -1,30 +1,37 @@
 import react from 'react';
 
-function Section2Part1()
+function Section2Box1()
 {
     return(
         <>
-        <div className='box1'>
+        <div className='box'>
            <div className='bg_image'>
                    <img src='images\promo_macbook_air_m3__e43jegok3wuq_large.jpg' width={'100%'}/>
            </div>
            <div className='text'>
-                
+                <h1>iPad</h1>
+                <h2>Lovable. Drawable. Magical</h2>
+                <button className="btn-1"><a href="#">learn more</a></button>
+                <button className="btn-2"><a href="#" >Buy</a></button>
            </div>
            </div>
         </>
     )
 }
-function Section2Part2()
+function Section2Box2()
 {
     return(
         <>
-        <div className='box2'>
+        <div className='box'>
            <div className='bg_image'>
-                   <img src='images\promo_macbook_air_m3__e43jegok3wuq_large.jpg' width={'100%'}/>
+                   <img src='images\promo_apple_watch_series_9_order__b3u85rm9zf6u_large.jpg' width={'100%'}/>
            </div>
            <div className='text'>
-                <img src='images\logo_tradein__d1fpktgipvki_large.png'></img>
+                <img src='images\logo_hero_light__d7t8cya4x26a_large.png'></img>
+                <h1></h1>
+                    <h2></h2>
+                    <button className="btn-1"><a href="#">Learn more</a></button>
+                    <button className="btn-2"><a href="#" >Buy</a></button>
            </div>
            </div>
         </>
@@ -37,8 +44,8 @@ function Section2Part2()
 function section2main() {
     return (
       <section className='section2'>
-         <Section2Part1></Section2Part1>
-         <Section2Part2></Section2Part2>
+         <Section2Box1></Section2Box1>
+         <Section2Box2></Section2Box2>
       </section>
     );
   }
